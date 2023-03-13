46ca42d0114de24ce4919b658c1266dae788577a
Cal Ellowitz <calellowitz@gmail.com>
2023-03-13 15:03:19 +0530

    add localsettings

---
+++ connectid/localsettings.example.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-yofpqrszrdtv0ftihjd09cuim2al9^n9j^b85%-y0v*^_lj18d'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
--- connectid/settings.py
+++ connectid/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yofpqrszrdtv0ftihjd09cuim2al9^n9j^b85%-y0v*^_lj18d'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -129,3 +123,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.ConnectUser'
+
+from localsettings import *
+
