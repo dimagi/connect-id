b004cad26c10c5a305f16390514edd488af5938a
Cal Ellowitz <calellowitz@gmail.com>
2023-03-29 09:38:50 +0530

    add phone verification views

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.test, name='test'),
     path('register', views.register, name='register'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('validate_phone', views.validate_phone, name='validate_phone'),
+    path('confirm_otp', views.confirm_otp, name='confirm_otp'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,7 +6,11 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from .models import ConnectUser
+from django_otp import match_token
+
+from .models import ConnectUser, PhoneDevice
+
+from utils import basicauth
 
 
 # Create your views here.
@@ -26,6 +30,37 @@ def register(request):
 def login(request):
     pass
 
+
 def test(request):
     return HttpResponse('pong')
 
+
+@require_POST
+@csrf_exempt
+@basicauth()
+def validate_phone(request):
+    # create otp device for user
+    # send otp code via twilio
+    print("hello")
+    user = request.user
+    otp_device, _ = PhoneDevice.objects.get_or_create(phone_number=user.phone_number, user=user)
+    otp_device.save()
+    otp_device.generate_challenge()
+    return HttpResponse()
+
+
+@require_POST
+@csrf_exempt
+@basicauth()
+def confirm_otp(request):
+    # check otp code for user
+    # mark phone as confirmed on user model
+    user = request.user
+    device = PhoneDevice.objects.get(phone_number=user.phone_number, user=user)
+    data = json.loads(request.body)
+    verified = device.verify_token(data.get('token'))
+    if not verified:
+        return JsonResponse({"error": "OTP token is incorrect"}, status=401)
+    user.phone_validated = True
+    user.save()
+    return HttpResponse()
