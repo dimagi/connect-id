210682126766f12f1335bd0f7b32e272de913d87
Cal Ellowitz <calellowitz@gmail.com>
2023-05-24 15:50:19 -0400

    add password strength validation

--- connectid/settings.py
+++ connectid/settings.py
@@ -84,6 +84,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'users.validators.EntropyPasswordValidator',
+        'OPTIONS': {
+            'min_strength': 2
+        }
+    },
 ]
 
 
