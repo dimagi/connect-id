eccfa049edd4f65d9c1ecb49806040664545e78f
Simon Kelly <skelly@dimagi.com>
2023-10-06 12:11:03 +0200

    initial view test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from users.models import ConnectUser
+
+
+@pytest.mark.django_db
+def test_registration(client):
+    response = client.post('/users/register', {
+        'username': 'testuser',
+        'password': 'testpass',
+        'phone_number': '+27734567657',
+    })
+    assert response.status_code == 200, response.content
+    user = ConnectUser.objects.get(username='testuser')
+    assert user.phone_number == '+27734567657'
