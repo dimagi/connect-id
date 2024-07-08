29c1b40af0a8822d39832e1b74cafc943285b4b7
Pawan Verma <pverma@dimagi.com>
2024-07-08 16:58:50 +0530

    Add is_active to userinfo, scope change for fields

--- users/oauth.py
+++ users/oauth.py
@@ -1,10 +1,17 @@
 from oauth2_provider.oauth2_validators import OAuth2Validator
 
+
 class ConnectOAuth2Validator(OAuth2Validator):
+    oidc_claim_scope = OAuth2Validator.oidc_claim_scope
+    oidc_claim_scope.update(
+        {"is_active": "openid", "phone": "openid", "name": "openid"}
+    )
 
     def get_additional_claims(self, request):
         claims = {}
         claims["sub"] = request.user.username
         claims["name"] = request.user.name
-        claims["phone"] = request.user.phone_number
+        claims["phone"] = request.user.phone_number.as_e164
+        claims["is_active"] = request.user.is_active
+        print(claims)
         return claims
