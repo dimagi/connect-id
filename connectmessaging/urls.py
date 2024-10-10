b8bc5b895bab4d4f7a4d7ec9195b3a2bc7d5c391
hemant10yadav <hemant.10.yadav@gmail.com>
2024-10-10 13:23:05 +0530

    Refeactor code

--- connectmessaging/urls.py
+++ connectmessaging/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('create_channel/', views.CreateChannelView.as_view(), name='create_channel'),
     path('send_message/', views.SendMessageView.as_view(), name='send_message'),
     path('update_consent/', views.UpdateConsentView.as_view(), name='update_consent'),
-    path('retrieve_messages/:connect_user_id', views.retrieve_message, name='retrieve_messages'),
+    path('retrieve_messages/:connect_user_id', views.RetrieveMessageView.as_view(), name='retrieve_messages'),
     path('update_received/', views.UpdateReceivedView.as_view(), name='update_received'),
 ]
--- connectmessaging/views.py
+++ connectmessaging/views.py
@@ -1,8 +1,9 @@
 import requests
 from django.db.models import Prefetch
 from django.utils import timezone
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from rest_framework import status, views
-from rest_framework.decorators import api_view
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
 
 from messaging.serializers import Message as Msg
@@ -16,16 +17,13 @@ class CommCareHQAPIException(Exception):
     pass
 
 
-class BaseAuthenticatedAPIView(views.APIView):
-    authentication_classes = [ClientProtectedResourceAuth]
-
-
 def is_request_from_hq(request):
-    host = request.META.get('HTTP_HOST')
+    host = request.META.get("HTTP_HOST")
     return host == "'commcarehq.org'"
 
 
-class CreateChannelView(BaseAuthenticatedAPIView):
+class CreateChannelView(views.APIView):
+    authentication_classes = [ClientProtectedResourceAuth]
 
     def post(self, request, *args, **kwargs):
         serializer = ChannelSerializer(data=request.data)
@@ -35,56 +33,77 @@ class CreateChannelView(BaseAuthenticatedAPIView):
                 usernames=[channel.connect_user.username],
                 title="Channel created",
                 body="A new channel has been created for you. Please provide your consent to proceed.",
-                data={"keyUrl": channel.key_url}
+                data={"keyUrl": channel.key_url},
             )
             send_bulk_message(message)
-            return Response({"channel_id": str(channel.channel_id)}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"channel_id": str(channel.channel_id)}, status=status.HTTP_201_CREATED
+            )
+        response = Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return response
 
 
-class SendMessageView(BaseAuthenticatedAPIView):
+class SendMessageView(views.APIView):
+
+    # Will be called from mobile and other services.
+    authentication_classes = [
+        BasicAuthentication,
+        OAuth2Authentication,
+        ClientProtectedResourceAuth,
+    ]
+
     def post(self, request, *args, **kwargs):
         serializer = MessageSerializer(data=request.data)
         if serializer.is_valid():
-            channel = serializer.validated_data.get('channel')
-            if not Channel.objects.filter(channel_id=channel.channel_id, user_consent=True).exists():
-                return Response({"error": "Consent is required for this channel"}, status=status.HTTP_403_FORBIDDEN)
+            channel = serializer.validated_data.get("channel")
+            if not Channel.objects.filter(
+                channel_id=channel.channel_id, user_consent=True
+            ).exists():
+                return Response(
+                    {"error": "Consent is required for this channel"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
 
             message = serializer.save()
 
             if is_request_from_hq(request):
                 send_bulk_message(message)
             else:
-                post_on_hq(channel.delivery_url, json_data=message)
+                make_request_to_service(channel.delivery_url, json_data=message)
 
-            return Response({"message_id": str(message.message_id)}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"message_id": str(message.message_id)}, status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-def retrieve_message(request):
-    user = request.user
-    channels = Channel.objects.filter(connect_user=user).only('channel_id', 'channel_source',
-                                                              'key_url').prefetch_related(
-        Prefetch('message_set', queryset=Message.objects.only('message_id', 'channel', 'timestamp'))
-    )
+class RetrieveMessageView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        channels = (
+            Channel.objects.filter(connect_user=user)
+            .only("channel_id", "channel_source", "key_url")
+            .prefetch_related(
+                Prefetch(
+                    "message_set",
+                    queryset=Message.objects.only("message_id", "channel", "timestamp"),
+                )
+            )
+        )
 
-    channels_data = ChannelSerializer(channels, many=True).data
+        channels_data = ChannelSerializer(channels, many=True).data
 
-    messages = []
-    for channel in channels:
-        channel_messages = channel.message_set.all()
-        messages.extend(channel_messages)
+        messages = []
+        for channel in channels:
+            channel_messages = channel.message_set.all()
+            messages.extend(channel_messages)
 
-    messages_data = MessageSerializer(messages, many=True).data
+        messages_data = MessageSerializer(messages, many=True).data
 
-    return Response({
-        "channels": channels_data,
-        "messages": messages_data
-    })
+        return Response({"channels": channels_data, "messages": messages_data})
 
 
-class UpdateConsentView(BaseAuthenticatedAPIView):
+class UpdateConsentView(views.APIView):
     def post(self, request, *args, **kwargs):
         channel_id = request.data.get("channel")
         consent = request.data.get("consent")
@@ -98,16 +117,18 @@ class UpdateConsentView(BaseAuthenticatedAPIView):
             url = "CONSENT_URL"
             json = {
                 "channel_id": str(channel.channel_id),
-                "consent": str(channel.user_consent)
+                "consent": str(channel.user_consent),
             }
-            post_on_hq(url, json)
+            make_request_to_service(url, json)
 
             return Response(status=status.HTTP_200_OK)
         except Channel.DoesNotExist:
-            return Response({"error": "Channel not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "Channel not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
 
-class UpdateReceivedView(BaseAuthenticatedAPIView):
+class UpdateReceivedView(views.APIView):
     def post(self, request, *args, **kwargs):
         message_ids = request.data.get("messages", [])
 
@@ -118,19 +139,29 @@ class UpdateReceivedView(BaseAuthenticatedAPIView):
 
         Message.objects.bulk_update(messages, ["received"])
 
-        if messages.exists():
-            post_on_hq(messages.first().channel.delivery_url,
-                       json_data=[message.message_id for message in messages])
+        if messages:
+            make_request_to_service(
+                messages.first().channel.delivery_url,
+                json_data=[
+                    {
+                        "message_id": message.message_id,
+                        "received": str(message.received),
+                    }
+                    for message in messages
+                ],
+            )
 
         return Response(status=status.HTTP_200_OK)
 
 
-def post_on_hq(url, json_data):
+def make_request_to_service(url, json_data):
     #: TO-DO add authorization.
-    headers = {'Content-Type': 'application/json'}
+    headers = {"Content-Type": "application/json"}
     try:
         response = requests.post(url, json=json_data, headers=headers)
         response.raise_for_status()
         return response
     except requests.exceptions.RequestException as e:
-        return CommCareHQAPIException({'status': 'error', 'message': str(e)}, )
+        return CommCareHQAPIException(
+            {"status": "error", "message": str(e)},
+        )
