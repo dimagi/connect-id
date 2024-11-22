cf6d2f64fb95421ab942574c65858b3d070f025c
Sravan Reddy <sravfeyn@gmail.com>
2024-11-22 15:06:48 +0530

    no need for singleton

--- utils/twilio.py
+++ utils/twilio.py
@@ -6,18 +6,12 @@ from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
-# Create the client instance only once
-twilio_client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-
-def get_twilio_client():
-    return twilio_client
-
 
 def lookup_telecom_provider(phone_number):
-    client = get_twilio_client()
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
     try:
         phone_info = client.lookups.v1.phone_numbers(phone_number).fetch(type="carrier")
         return phone_info.carrier.get("name")
     except Exception as e:
-        logger.exception("Error occurred during twilio call: %s", str(e))
+        logger.exception("Error occurred during Twilio call for phone number %s: %s", phone_number, str(e))
         return None
