d4212ab54294b3bc92fb982bb871d3a66a50b29f
Cal Ellowitz <calellowitz@gmail.com>
2024-12-15 15:01:07 -0500

    fix migration conflict

--- users/migrations/0012_phonedevice_otp_last_sent.py
+++ users/migrations/0012_phonedevice_otp_last_sent.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("users", "0008_credential_usercredential"),
+        ("users", "0011_alter_connectuser_phone_number"),
     ]
 
     operations = [
