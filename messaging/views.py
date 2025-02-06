bd030c0b5590222c80e1948c489226b41a9df406
Cal Ellowitz <calellowitz@gmail.com>
2025-02-06 15:06:42 +0000

    make sure all fields are strings

--- messaging/views.py
+++ messaging/views.py
@@ -182,11 +182,11 @@ class CreateChannelView(APIView):
                 title="Channel created",
                 body="Please provide your consent to send/receive message.",
                 data={
-                    "key_url": server.key_url,
+                    "key_url": str(server.key_url),
                     "action": CCC_MESSAGE_ACTION,
                     "channel_source": channel_source,
                     "channel_id": str(channel.channel_id),
-                    "consent": channel.user_consent
+                    "consent": str(channel.user_consent)
                 },
             )
             # send fcm notification.
