a89477cc7219f9d341b9afeaf1ad8393edab42e2
Charl Smit <csmit@dimagi.com>
2025-04-22 17:17:24 +0200

    Check individual response verdicts

--- utils/app_integrity/google_play_integrity.py
+++ utils/app_integrity/google_play_integrity.py
@@ -1,3 +1,6 @@
+from utils.app_integrity.schemas import AccountDetails, AppIntegrity, DeviceIntegrity, RequestDetails, VerdictResponse
+
+
 class AppIntegrityService:
     """
     Verifies the application integrity of the app using Google Play Integrity API.
@@ -11,13 +14,28 @@ class AppIntegrityService:
         verdict_response = self._obtain_verdict()
         self._analyze_verdict(verdict_response)
 
-    def _obtain_verdict(self):
+    def _obtain_verdict(self) -> VerdictResponse:
         """Obtain token verdict from Google"""
         pass
 
-    def _analyze_verdict(self, verdict_response):
+    def _analyze_verdict(self, verdict: VerdictResponse):
         """
         Checks the verdict and raises appropriate exceptions if
         the app integrity is compromised.
         """
+        self._check_request_details(verdict.requestDetails)
+        self._check_app_integrity(verdict.appIntegrity)
+        self._check_device_integrity(verdict.deviceIntegrity)
+        self._check_account_details(verdict.accountDetails)
+
+    def _check_request_details(self, request_details: RequestDetails):
+        pass
+
+    def _check_app_integrity(self, app_integrity: AppIntegrity):
+        pass
+
+    def _check_device_integrity(self, device_integrity: DeviceIntegrity):
+        pass
+
+    def _check_account_details(self, account_details: AccountDetails):
         pass
