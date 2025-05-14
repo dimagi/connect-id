32a9b70c973038577314366965dc3c5f31edf946
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-05-14 15:38:03 +0200

    remove boilerplate comment

--- users/views.py
+++ users/views.py
@@ -26,7 +26,6 @@ from .fcm_utils import create_update_device
 from .models import ConnectUser, Credential, PhoneDevice, RecoveryStatus, UserCredential, UserKey
 
 
-# Create your views here.
 @api_view(["POST"])
 @permission_classes([])
 def register(request):
