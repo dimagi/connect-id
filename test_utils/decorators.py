1c866e440e8339ae3b60412aa268e9ea438f5b3b
Cal Ellowitz <calellowitz@gmail.com>
2025-06-23 22:54:22 -0400

    mock check number when skipping integrity check

--- test_utils/decorators.py
+++ test_utils/decorators.py
@@ -8,7 +8,8 @@ def skip_app_integrity_check(test_func):
     @functools.wraps(test_func)
     def wrapper(*args, **kwargs):
         with mock.patch.object(AppIntegrityService, "verify_integrity") as verify_integrity_mock:
-            verify_integrity_mock.return_value = None
-            return test_func(*args, **kwargs)
+            with mock.patch("utils.app_integrity.decorators.check_number_for_existing_invites"):
+                verify_integrity_mock.return_value = None
+                return test_func(*args, **kwargs)
 
     return wrapper
