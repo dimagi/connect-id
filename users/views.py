8cc960494257ca226444157c4c81717bc0bc6c3b
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-06-24 11:45:28 +0200

    block users if account locked

--- users/views.py
+++ users/views.py
@@ -70,6 +70,12 @@ def start_device_configuration(request):
     logger.info(f"Start configuration for phone: {data}")
     if not data.get("phone_number"):
         return JsonResponse({"error_code": ErrorCodes.MISSING_DATA}, status=400)
+    try:
+        user = ConnectUser.objects.get(phone_number=data["phone_number"], is_active=False, is_locked=True)
+        if user:
+            return JsonResponse({"error_code": ErrorCodes.LOCKED_ACCOUNT}, status=403)
+    except ConnectUser.DoesNotExist:
+        pass
 
     is_demo_user = data["phone_number"].startswith(TEST_NUMBER_PREFIX)
 
