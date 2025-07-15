19d557dd02fefd227c501afa06874a628bd324b4
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-07-15 10:54:46 +0200

    Revert "make app_id required field"
    This reverts commit 23e3dc1fc19f6729a3648e2b12e89df17ae1a559.

--- users/migrations/0022_rename_name_credential_title_and_more.py
+++ users/migrations/0022_rename_name_credential_title_and_more.py
@@ -49,7 +49,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="credential",
             name="app_id",
-            field=models.CharField(max_length=50),
+            field=models.CharField(blank=True, max_length=50, null=True),
         ),
         migrations.AddField(
             model_name="credential",
